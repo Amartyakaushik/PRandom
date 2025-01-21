d6be5bf0b0c4fbebb898a71d8cef17e46df88d58
Amartya <kamartya58@gmail.com>
2025-01-21 20:54:50 +0530

    LeetCode Question 2326(Spiral Matrix IV)

---
+++ LC/2326SpiralMatrixIV.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+vector<vector<int>> spiralMatrix(int n, int m, ListNode* head) {
+// n*m
+int sr = 0;
+int sc = 0;
+int direction = 0;
+vector<vector<int>> arr(n, vector<int>(m, -1));
+while(head){
+arr[sr][sc] = head->val;
+head = head->next;
+if(direction == 0){
+sc++;
+if(sc == m or arr[sr][sc] != -1){
+sc--;
+sr++;
+direction = 1;
+}
+}
+else if(direction == 1){
+sr++;
+if(sr == n or arr[sr][sc] != -1){
+sr--;
+sc--;
+direction = 2;
+}
+}
+else if(direction == 2){
+sc--;
+if(sc < 0 or arr[sr][sc] != -1){
+sr--;
+sc++;
+direction = 3;
+}
+}
+else if(direction == 3){
+sr--;
+if(sr < 0 or arr[sr][sc] != -1){
+sr++;
+sc++;
+direction = 0;
+}
+}
+}
+return arr;
+}
+};
